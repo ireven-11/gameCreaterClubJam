24ea7fa55ab1710542bcb714be55d77da95bf38d
Roll <setokazuma0617@gmail.com>
2026-03-11 14:13:54 +0900

    移動機能を追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region シリアライズフィールド群
+    [SerializeField] private Status status = new Status();
+    #endregion
+
+    #region メンバ変数群
+    private Vector2 _currentMoveDir = Vector2.zero;
+    #endregion
+
+    #region 関数群
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var nextMoveDir = Vector2.zero;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            nextMoveDir.y += 1;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            nextMoveDir.y -= 1;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            nextMoveDir.x += 1;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            nextMoveDir.x -= 1;
+        }
+        nextMoveDir.Normalize();
+
+        _currentMoveDir = Vector2.Lerp(_currentMoveDir, nextMoveDir, 0.5f);
+        var addPos = _currentMoveDir * status.speed * Time.deltaTime;
+        transform.position += new Vector3(addPos.x, addPos.y, 0.0f);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Status
+{
+    public float speed = 0.0f;
+    public float attack = 0.0f;
+
+    public Status(float speed, float attack)
+    {
+        this.speed = speed;
+        this.attack = attack;
+    }
+
+    public Status()
+    {
+        this.speed = 0.0f;
+        this.attack = 0.0f;
+    }
+}

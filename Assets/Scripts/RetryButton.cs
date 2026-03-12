5ab8ee2d5c2c1cf0023868fee228a023a4424a6b
ireven <hensati11@gmail.com>
2026-03-12 16:47:51 +0900

    seを鳴らそうとした

--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RetryButton : MonoBehaviour
 {
     [SerializeField] Button button;
+
+    public AudioClip se1;
+    AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +27,9 @@ public class RetryButton : MonoBehaviour
     {
         //todo：後でシーンの名前を正式名称に変える
         SceneManager.LoadScene("InStage");
+
+        audioSource = GetComponent<AudioSource>(); //Componentを取得
+        audioSource.PlayOneShot(se1);
 
         //Debug.Log("リトライボタンが押された");
     }

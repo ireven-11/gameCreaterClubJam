cc6d4a564d7648f86a43b67feb3656a0db15d97a
ireven <hensati11@gmail.com>
2026-03-13 09:44:54 +0900

    フェード処理を両方のボタンで実装（バグあり）

--- Assets/Scripts/ReturnToTitleButton.cs
+++ Assets/Scripts/ReturnToTitleButton.cs
@@ -5,24 +5,39 @@ using UnityEngine.SceneManagement;
 public class ReturnToTitleButton : MonoBehaviour
 {
     [SerializeField] Button button;
+    [SerializeField] private Fade fade;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        button.onClick.AddListener(() => { Onclick(); });
+        button.onClick.AddListener(OnClick);
+        fade.OnFadeOutComplete += OnFadeOutFinished;
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (fade != null)
+        {
+            fade.OnFadeOutComplete -= OnFadeOutFinished;
+        }
     }
 
-    private void Onclick()
+    private void OnClick()
     {
-        //todo：後でシーンの名前を正式名称に変える
-        SceneManager.LoadScene("title");
+        if (fade == null)
+        {
+            //Debug.LogError("Fade がインスペクタで設定されていません");
+            return;
+        }
 
-        //Debug.Log("タイトルへボタンが押された");
+        //Debug.Log("RetryButton: StartFadeOut 呼び出し");
+        fade.StartFadeOut();
+    }
+
+    private void OnFadeOutFinished()
+    {
+        //Debug.Log("RetryButton: フェードアウト完了 → シーン遷移");
+        SceneManager.LoadScene("title");
     }
 }
